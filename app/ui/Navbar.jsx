d02d2cd8f855a1863b5baa0eb88c893775eb2310
dmostafiz <dev.mostafiz@gmail.com>
2023-09-28 10:12:10 +0600

    ok

--- app/ui/Navbar.jsx
+++ app/ui/Navbar.jsx
@@ -49,28 +49,6 @@ const Navbar = () => {
   return (
     <Stack bg="#071c09" justify="space-evenly">
       <Container maxW="1180px">
-        <HStack justify="space-between" h="72px">
-          <Flex>
-            <Img src="\icons\nav-icon.svg" />
-            <Text color="white" fontSize="14px" fontWeight="medium" ml="4">
-              Free Delivery When you buy Order above
-              <Text as="span" fontFamily="default" ml="1">
-                $
-              </Text>
-              100
-            </Text>
-          </Flex>
-          <Flex gap="5">
-            {navIcon.map((icon) => (
-              <Link target="_blank" href={icon.url}>
-                <Image src={icon.icon} />
-              </Link>
-            ))}
-          </Flex>
-        </HStack>
-      </Container>
-      <Divider bg="gray.800" />
-      <Container maxW="1180px">
         <HStack justify="space-between" h="94px">
           <Flex>
             <Text color="brand.600" fontSize="24px" fontWeight="bold">
